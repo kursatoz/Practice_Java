b409dad199e00f8a11ed7e08641b23e0dc3740e8
OzdumanKursad <kursad.ozduman@gmail.com>
2023-04-02 12:41:15 -0400

    duplication program solved

--- src/com/company/day9/RemoveLetterPractice.java
+++ src/com/company/day9/RemoveLetterPractice.java
@@ -1,2 +1,29 @@
-package com.company.day9;public class RemoveLetterPractice {
+package com.company.day9;
+
+public class RemoveLetterPractice {
+
+    public static void main(String[] args) {
+
+        /**
+         * 012345678901234567890123  delete all duplicated letter
+         * AAABBDNDD DaSZO@I 6 654@@
+         * ABDN aSZ0@I654
+         *
+         */
+        String str = "AAABBDNDD DaSZO@I 6 654@@";
+        String nonDuplicated = "";
+        for (int i = 0; i <str.length() ; i++) {
+            if (!nonDuplicated.contains(str.substring(i,i+1))){
+                nonDuplicated+=str.charAt(i);
+
+            }
+
+
+        }
+        System.out.println(str);
+        System.out.println(nonDuplicated);
+
+    }
+
+
 }
--- src/com/company/day9/practice3.java
+++ src/com/company/day9/practice3.java
@@ -1,2 +1,19 @@
-package com.company.day9;public class practice3 {
+package com.company.day9;
+
+public class practice3 {
+    public static void main(String[] args) {
+
+        String text ="AAA";
+        String newtext ="";
+        for (int i = 0; i <text.length() ; i++) {
+                if (!newtext.contains(text.substring(i,i+1))){
+
+                    newtext+=text.charAt(i);
+                }
+
+        }
+        System.out.println(newtext);
+    }
+
+
 }
